b836cf0a77c1f9e3f0a643f5553cf4599698dc57
Ethan43443 <125399829+Ethan43443@users.noreply.github.com>
2024-04-01 15:31:01 -0400

    Update wBCamera.py

--- wBCamera.py
+++ wBCamera.py
@@ -4,13 +4,13 @@ from time import sleep
 
 photoCount=0
 #This is for when we manage photos
-def takePicture(cameraObj,seconds=2,photos=112):
+def takePicture(cameraObj,seconds=2,photos=112,resolution=(2592, 1944)):
     """
     cameraObj=A camera object to take a picture with 
     seconds = Must be over 2 seconds. Takes a picture.
     """
     photo_path='/home/pi/Desktop/image%s.jpg'
-    cameraObj.resolution = (2592, 1944)
+    cameraObj.resolution = resolution
     cameraObj.framerate = 15
     if(seconds >= 2):
         cameraObj.start_preview()
@@ -25,11 +25,11 @@ def takePicture(cameraObj,seconds=2,photos=112):
     else:
         return "Photo not taken because seconds given is: "+str(seconds)
     #Solar Eclipse time 3 minutes 45 seconds -> 225/2 -> 112 Pictures approximately
-def takeVideo(cameraObj,seconds=225):
+def takeVideo(cameraObj,seconds=225,resolution=(1920, 1080)):
     #Note: Takes about 5GB for OS system. 16 GB for the entire SD card = 12.5GB
     #
     #seconds defaults to the amount of time during the solar eclipse
-    cameraObj.resolution = (1920, 1080)
+    cameraObj.resolution = resolution
     cameraObj.framerate = 120
     video_path='/home/pi/Desktop/video.h264'
     cameraObj.start_preview()
@@ -40,5 +40,6 @@ def takeVideo(cameraObj,seconds=225):
 def main():
     camera = PiCamera()
     takePicture(camera,seconds=2,photos=5)
+    takeVideo(camera,seconds=10)
 #Burn wire: If the balloon is stuck at a certain altitude of x amount of seconds then cut the rope
     
