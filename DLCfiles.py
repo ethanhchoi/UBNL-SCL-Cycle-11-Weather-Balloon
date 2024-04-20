c38cdb9b86acd403316a18a0f25ca29355c4457c
Ethan43443 <125399829+Ethan43443@users.noreply.github.com>
2024-04-20 16:25:44 -0400

    Update DLCfiles.py

--- DLCfiles.py
+++ DLCfiles.py
@@ -1,4 +1,7 @@
-import cv2
+from PIL import Image
+import os
+temp_path="testhahaha.png"
+import PIL.ImageShow
 #3840x2160
 def imageSize(size):
   if(size=="HD"):
@@ -6,8 +9,19 @@ def imageSize(size):
     return (1920,1080)
   elif(size=="4K"):
     return (3840,2160)
-def enlargenImages(folder_path_dir,photo_size=(3840,2160)):
+def alterImage(image_path,size):
+  """
+  Purpose: Alters an image and adds "_" to indicate that it was copied
+  """
+  img=Image.open(image_path)
+  changed_img=img.resize((size))
+  changed_img.save("_"+image_path)
+def changeImages(folder_path_dir,photo_size=(3840,2160)):
+  """
+  Given a directory of images change based on quality
+  """
   for images in os.listdir(folder_path_dir):
-    img=cv2.imread(images)
-    cv2.resize(img,photo_size)
+    alterImage(images,photo_size)
+#alterImage("rasberryPI_img_2.jpg",imageSize("4K"))
+changeImages("ImageFolder")
 #def checkStorage(folder_path)
