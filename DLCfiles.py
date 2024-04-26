59d62958ac6d9c5afb7939572dfee211018c8821
Ethan43443 <125399829+Ethan43443@users.noreply.github.com>
2024-04-25 23:19:20 -0400

    Update DLCfiles.py -Updating with 4:3 ratio

--- DLCfiles.py
+++ DLCfiles.py
@@ -4,11 +4,17 @@ temp_path="testhahaha.png"
 import PIL.ImageShow
 #3840x2160
 def imageSize(size):
-  if(size=="HD"):
+  if(size=="XGA+"):
     #1920x1080
-    return (1920,1080)
-  elif(size=="4K"):
-    return (3840,2160)
+    return (1152,864)
+  elif(size=="QXGA"):
+    return (2048,1536)
+  elif(size=="QUXGA"):
+    return (3200,2400)
+  elif(size=="HXGA"):
+    return (4096,3072)
+  elif(size=="HUXGA"):
+    return (6400,4800)
 def alterImage(image_path,size):
   """
   Purpose: Alters an image and adds "_" to indicate that it was copied
@@ -18,8 +24,8 @@ def alterImage(image_path,size):
   changed_img.save("_"+image_path)
 def increaseRatio(image_path,ratio_increase):
   img=Image.open(image_path)
-  width*ratio_increase,height*ratio_increase=img.size
-  changed_img=img.resize((width,height))
+  width,height=img.size
+  changed_img=img.resize((width*ratio_increase,height*ratio_increase))
   changed_img.save("_"+image_path)
 def changeImages(folder_path_dir,photo_size=(3840,2160)):
   """
@@ -27,6 +33,8 @@ def changeImages(folder_path_dir,photo_size=(3840,2160)):
   """
   for images in os.listdir(folder_path_dir):
     alterImage(images,photo_size)
-#alterImage("rasberryPI_img_2.jpg",imageSize("4K"))
-changeImages("ImageFolder")
+alterImage("rasberryPI_img_0.jpg",imageSize("HUXGA"))
+#increaseRatio("rasberryPI_img_2.jpg",4)
+#(640,480)
+#changeImages("ImageFolder")
 #def checkStorage(folder_path)
